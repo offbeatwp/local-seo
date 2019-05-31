6a9cc7cbb7ce6c5b9c9c643e85c6986122af0637
Daan Seegers <daan@raow.nl>
2019-05-31 13:29:11 +0200

    Beta

---
+++ composer.js
@@ -0,0 +1,22 @@
+{
+    "name": "offbeatwp/local-seo",
+    "description": "Adding some GravityForms functionality",
+    "license": "MIT",
+    "type": "offbeatwp-service",
+    "authors": [
+    {
+        "name": "Chris Schrijver",
+        "email": "chris@raow.nl"
+    },
+    {
+        "name": "Daan Seegers",
+        "email": "daan@raow.nl"
+    }
+],
+    "autoload": {
+    "psr-4": {
+        "OffbeatWP\\LocalSeo\\": "src"
+    }
+},
+    "require": {}
+}

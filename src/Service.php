601fce92e841e70c0f4cbd4b28deca10749994b9
Daan Seegers <d.seegers.mail@gmail.com>
2019-06-03 15:28:35 +0200

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -1,3 +1,19 @@
 <?php
 
-namespace OffbeatWP\LocalSeo;
+namespace App\Contracts\LocalSeo;
+
+use OffbeatWP\Services\AbstractService;
+
+class SiteService extends AbstractService
+{
+    public function register()
+    {
+        offbeat('post-type')->registerPostModel('post', \App\Models\PostModel::class);
+        offbeat('post-type')->registerPostModel('page', \App\Models\PageModel::class);
+
+        add_theme_support('custom-header');
+        add_theme_support('post-thumbnails');
+
+    }
+
+}

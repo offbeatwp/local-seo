8d6535c00d883fb777c6ac23a5d5a65adc41395f
Daan Seegers <d.seegers.mail@gmail.com>
2019-06-03 15:55:58 +0200

    Update LocalSeoService.php

---
+++ src/LocalSeoService.php
@@ -0,0 +1,23 @@
+
+
+<?php
+namespace OffbeatWP\LocalSeo;
+
+use OffbeatWP\Services\AbstractService;
+use OffbeatWP\Contracts\SiteSettings;
+
+
+class LocalSeoService extends AbstractService
+{
+    const ID = 'seo-settings';
+
+    protected $settings;
+
+    public function register(SiteSettings $settings)
+    {
+
+        $settings->addPage(\App\SiteSettings\SettingsScripts::class);
+
+    }
+}
+

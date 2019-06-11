dd4ab7da108045416379a223af5232095f18b5bb
Daan Seegers <daan@raow.nl>
2019-06-11 12:08:40 +0200

    Add some more static functions for the data

--- src/Data/DateTime.php
+++ src/Data/DateTime.php
@@ -39,5 +39,20 @@ class DateTime
 
     }
 
+    public static function Days(){
+
+        $days = [
+            'Monday' => 'Monday',
+            'Tuesday' => 'Tuesday',
+            'Wednesday' => 'Wednesday',
+            'Thursday' => 'Thursday',
+            'Friday' => 'Friday',
+            'Saturday' => 'Saturday',
+            'Sunday' => 'Sunday',
+        ];
+
+            return $days;
+    }
+
 
 }

7a90b22f591626e2e24c8794025d3373b58e5a36
Daan Seegers <daan@raow.nl>
2019-06-11 11:50:43 +0200

    Call datetime as static function

--- src/AddSeoFields.php
+++ src/AddSeoFields.php
@@ -199,41 +199,14 @@ class AddSeoFields
         ]);
 
 
-        $time = [
-            '7:00' => '7:00',
-            '8:00' => '8:00',
-            '9:00' => '9:00',
-            '10:00' => '10:00',
-            '11:00' => '11:00',
-            '12:00' => '12:00',
-            '13:00' => '13:00',
-            '14:00' => '14:00',
-            '15:00' => '15:00',
-            '16:00' => '16:00',
-            '17:00' => '17:00',
-            '18:00' => '18:00',
-            '19:00' => '19:00',
-            '20:00' => '20:00',
-            '21:00' => '21:00',
-            '22:00' => '22:00',
-            '23:00' => '23:00',
-            '24:00' => '24:00',
-            '1:00' => '1:00',
-            '2:00' => '2:00',
-            '3:00' => '3:00',
-            '4:00' => '4:00',
-            '5:00' => '5:00',
-            '6:00' => '6:00',
-        ];
-
         $countries =  \OffbeatWP\Form\Fields\Select::make('countries', 'Countries');
 
         $countries->addOptions(\OffbeatWP\LocalSeo\data\Location::country());
 
         $closingTime = \OffbeatWP\Form\Fields\Select::make('closing_time', 'Closing time');
         $openingHours = \OffbeatWP\Form\Fields\Select::make('opening_time', 'Opening time');
-        $closingTime->addOptions($time);
-        $openingHours->addOptions($time);
+        $closingTime->addOptions(\OffbeatWP\LocalSeo\data\DateTime::Time());
+        $openingHours->addOptions(\OffbeatWP\LocalSeo\data\DateTime::Time());
         $form->addField($localCompanyKind);
         $form->addTab('contact-information', 'Contact details');
         $form->addField(\OffbeatWP\Form\Fields\Text::make('company_fax', 'Fax number'));

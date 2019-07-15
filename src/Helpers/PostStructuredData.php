4bc307f9b604a777e42f128f1b3e2efe743059eb
Daan Seegers <daan@raow.nl>
2019-07-15 16:16:27 +0200

    I have disabled the fields for post_types (since we don' t have to use them yet)

--- src/Helpers/PostStructuredData.php
+++ src/Helpers/PostStructuredData.php
@@ -8,30 +8,30 @@ class PostStructuredData
     public function __construct()
     {
 
-        $acfField = new MakeAcfFieldsForStructuredData('Structured Data');
-
-//      New article
-        $acfField->addType('Article', 'tab_article');
-        $acfField->addTextField('Headline', 'head_line');
-        $acfField->addTextField('Page Url', 'page_url');
-        $acfField->addTextField('Author', 'author');
-        $acfField->addDateField('Datumm', 'te_gekke_datum');
-        $acfField->addUrlField('Linkje', 'aa_aaa_aa');
-
-//      Blog post
-
-        $acfField->addType('Blog Post', 'tab_blog_post');
-
-//      Event
-
-        $acfField->addType('Event', 'tab_event');
-
-//      Service
-
-        $acfField->addType('Service', 'tab_service');
-
-
-        $acfField->execute();
+//        $acfField = new MakeAcfFieldsForStructuredData('Structured Data');
+//
+////      New article
+//        $acfField->addType('Article', 'tab_article');
+//        $acfField->addTextField('Headline', 'head_line');
+//        $acfField->addTextField('Page Url', 'page_url');
+//        $acfField->addTextField('Author', 'author');
+//        $acfField->addDateField('Datumm', 'te_gekke_datum');
+//        $acfField->addUrlField('Linkje', 'aa_aaa_aa');
+//
+////      Blog post
+//
+//        $acfField->addType('Blog Post', 'tab_blog_post');
+//
+////      Event
+//
+//        $acfField->addType('Event', 'tab_event');
+//
+////      Service
+//
+//        $acfField->addType('Service', 'tab_service');
+//
+//
+//        $acfField->execute();
 
     }
 
